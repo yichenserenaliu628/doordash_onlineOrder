ec1a1a73db09c4270e4ea9dbff2e3537fc391c81
Serena Liu <serenaliu628@Serenas-MacBook-Pro-5.local>
2023-01-13 16:57:19 -0600

    My first commit

---
+++ src/main/java/com/laioffer/onlineorder/service/CustomerService.java
@@ -0,0 +1,35 @@
+package com.laioffer.onlineorder.service;
+
+import com.laioffer.onlineorder.dao.CustomerDao;
+import com.laioffer.onlineorder.entity.Cart;
+import com.laioffer.onlineorder.entity.Customer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomerService {
+
+    private CustomerDao customerDao;
+
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public CustomerService(CustomerDao customerDao, PasswordEncoder passwordEncoder) {
+        this.customerDao = customerDao;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public void signUp(Customer customer) {
+        Cart cart = new Cart();
+        customer.setCart(cart);
+
+        customer.setEnabled(true);
+        customer.setPassword(passwordEncoder.encode(customer.getPassword()));
+        customerDao.signUp(customer);
+    }
+
+    public Customer getCustomer(String email) {
+        return customerDao.getCustomer(email);
+    }
+}
